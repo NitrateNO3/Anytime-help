223108912ad03be6b3d63bed71acb36f0fa3f5e8
AmanMirza9898 <aman9811269898@gmail.com>
2026-08-31 14:38:14 +0530

    fix unused React import

--- admin-web/src/pages/ServiceBookings.tsx
+++ admin-web/src/pages/ServiceBookings.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ClipboardList, Clock, CheckCircle, AlertCircle, PlayCircle } from 'lucide-react';
 import toast from 'react-hot-toast';
